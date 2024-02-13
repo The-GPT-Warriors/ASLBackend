7c8baeab879968aae731daa616057a7eda765015
Ant11234 <anthonyb11c234@gmail.com>
2024-02-13 08:41:10 -0800

    AIModel Prediction Backend

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/aslAI/PredictionApiController.java
@@ -0,0 +1,28 @@
+package com.nighthawk.spring_portfolio.mvc.aslAI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+public class PredictionApiController {
+
+    @Autowired
+    private PredictionRepository predictionRepository;
+
+    @PostMapping("/aslresult")
+    public ResponseEntity<String> postPrediction(@RequestBody String predictionData) {
+        Prediction prediction = new Prediction();
+        prediction.setPredictionData(predictionData);
+        predictionRepository.save(prediction);
+        return ResponseEntity.ok("Prediction saved successfully!");
+    }
+
+
+    @GetMapping("/prediction")
+    public List<Prediction> getPredictionData() {
+        return predictionRepository.findAll();
+    }
+}
+
